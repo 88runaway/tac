b207bbe77809062cec8c6b6cc353ae1e954a1908
88runaway <ian3133295032@gmail.com>
2026-06-22 07:54:26 +0000

    delete_wandb

--- policy/Pi05_openpi_DF/sh/train.sh
+++ policy/Pi05_openpi_DF/sh/train.sh
@@ -17,7 +17,7 @@ python policy/Pi05_openpi_DF/train_df.py \
     --resume
 
 # 1. 数据转换（带触觉）
-python policy/Pi05_openpi_DF/convert_df_tactile.py --task insert_HDMI
+python policy/Pi05_openpi_DF/convert_df_tactile.py --task lift_can
 
 
 # 2. 训练（仅启用触觉 condition）
@@ -29,6 +29,13 @@ python policy/Pi05_openpi_DF/train_df.py --task insert_HDMI --gpu 3,4,5,6 \
 python policy/Pi05_openpi_DF/train_df.py --task insert_HDMI --gpu 4,5,6,7 \
     --use_tactile true --use_tactile_expert true \
     --tactile_expert_loss_weight 0.5 \
+    --block_time_sampling monotone --mix_prob 1.0 \
+    --warm_start_ckpt /data/zjb/ckpts/pi05_all_128_20k/params --overwrite
+
+
+python policy/Pi05_openpi_DF/train_df.py --task insert_HDMI --gpu 0,1,2,3 \
+    --use_tactile true --use_tactile_expert true \
+    --tactile_expert_loss_weight 1.0 \
     --block_time_sampling monotone --mix_prob 1.0 \
     --warm_start_ckpt /data/zjb/ckpts/pi05_all_128_20k/params --overwrite
 
